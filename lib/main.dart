e418f4fbb8d3c6d4f5281d2671feceb8687483ab
Theo Gkanoudis <thodoris.ganoudis@gmail.com>
2022-01-14 13:25:34 +0200

    added categories

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,50 @@ void main() {
   runApp(MyApp());
 }
 
+List<OutlinedButton> getCategories() {
+  List<String> categories = [
+    'Λογοτεχνία',
+    'Ιστορία',
+    'Φιλοσοφία',
+    'Τέχνη',
+    'Επιστήμη',
+    'Πολιτική',
+    'Αρχαία Ελληνική Γραμματεία',
+    'Παιδικά',
+    'Θρησκεία',
+    'Κοινωνία',
+    'Βιογραφία',
+    'Διατροφή',
+    'Υγεία',
+    'Εκπαίδευση',
+    'Μαγειρική',
+    'Ποίηση'
+  ];
+
+  final categoryButtons = <OutlinedButton>[];
+  for (var i = 0; i < categories.length; i++) {
+    categoryButtons.add(OutlinedButton(
+      onPressed: () {
+        debugPrint(categories[i]);
+      },
+      style: OutlinedButton.styleFrom(
+        side: const BorderSide(
+          width: 2.0,
+          color: Color.fromRGBO(49, 32, 73, 1.0),
+          style: BorderStyle.solid,
+        ),
+      ),
+      child: Text(categories[i],
+          style: const TextStyle(
+            color: Color.fromRGBO(0, 0, 0, 0.8),
+            fontWeight: FontWeight.w400,
+            fontSize: 17,
+          )),
+    ));
+  }
+  return categoryButtons;
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,7 +68,7 @@ class MyApp extends StatelessWidget {
                 child: Row(
                   children: [
                     Container(
-                      margin: const EdgeInsets.only(left: 20),
+                      padding: const EdgeInsets.all(20),
                       child: Column(
                         verticalDirection: VerticalDirection.up,
                         children: [
@@ -44,9 +88,9 @@ class MyApp extends StatelessWidget {
                             child: const Text(
                               'Καλωσήρθες,',
                               style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w600),
+                                color: Colors.white,
+                                fontSize: 28,
+                              ),
                             ),
                           ),
                         ],
@@ -69,26 +113,67 @@ class MyApp extends StatelessWidget {
               ),
             ),
             Container(
-              padding: const EdgeInsets.all(30),
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.only(top: 10),
               child: const TextField(
-                style: TextStyle(fontWeight: FontWeight.w600, color: Color.fromRGBO(49, 32, 73, 1.0),),
-                decoration: InputDecoration(
-                prefixIcon:  Icon(Icons.search, color: Color.fromRGBO(49, 32, 73, 1.0),),
-                hintText: "Αναζήτηση",
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Color.fromRGBO(255, 137, 0, 1.0), width: 2.0, style: BorderStyle.solid),
-                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: Color.fromRGBO(49, 32, 73, 1.0),
                 ),
+                decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Color.fromRGBO(49, 32, 73, 1.0),
+                  ),
+                  hintText: "Αναζήτηση",
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Color.fromRGBO(255, 137, 0, 1.0),
+                        width: 2.0,
+                        style: BorderStyle.solid),
+                    borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                  ),
                   focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(color: Color.fromRGBO(49, 32, 73, 1.0), width: 2.0, style: BorderStyle.solid,),
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(49, 32, 73, 1.0),
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                    ),
                     borderRadius: BorderRadius.all(Radius.circular(12.0)),
                   ),
                   border: OutlineInputBorder(
-                    borderSide: BorderSide(width: 2.0, style: BorderStyle.solid,),
+                    borderSide: BorderSide(
+                      width: 2.0,
+                      style: BorderStyle.solid,
+                    ),
                     borderRadius: BorderRadius.all(Radius.circular(12.0)),
                   ),
                 ),
+              ),
             ),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: const [
+                      Text(
+                        'Κατηγορίες Βιβλίων',
+                        style: TextStyle(
+                            color: Color.fromRGBO(49, 32, 73, 1.0),
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600),
+                        textAlign: TextAlign.left,
+                      ),
+                    ],
+                  ),
+                  Wrap(
+                    children: getCategories(),
+                    spacing: 10,
+                  ),
+                ],
+              ),
             ),
           ],
         ),
